b4eed17ea957b359f10f2b920a78509dc6b544ab
lucaswerner90 <wernerlucas12@gmail.com>
2019-02-05 19:54:48 +0100

    Try without the client

--- microservices/client/server.js
+++ microservices/client/server.js
@@ -3,7 +3,7 @@ const compression = require('compression');
 const next = require('next');
 const cookieParser = require('cookie-parser');
 
-const dev = true;
+const dev = process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 
 
